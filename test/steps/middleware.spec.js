94e0c35df21be09aeb56d054f9bdb8d626c20594
Adrien SAUNIER <asaunier@octo.com>
2017-01-10 16:03:03 +0100

    feat(#10): Creating a new middleware for step

---
+++ test/steps/middleware.spec.js
@@ -0,0 +1,74 @@
+const config    = require('config')
+const sinon     = require('sinon')
+const mongoose  = require('mongoose')
+const Faker     = require('faker')
+const httpMocks = require('node-mocks-http')
+const Step      = require(config.get('app-folder') + '/steps/model')
+
+const tripMiddleware = require(config.get('app-folder') + '/steps/middleware')
+
+describe('Step', () => {
+  describe('middleware', () => {
+    describe('exists', () => {
+      describe('when the steps does not exist', () => {
+        it('should return NOT_FOUND', (done) => {
+          // Given
+          var next = sinon.spy()
+          let response = httpMocks.createResponse()
+          let request = httpMocks.createRequest({
+            url: '',
+            params: {
+              tripid: mongoose.Types.ObjectId().toString(),
+              stepid: mongoose.Types.ObjectId().toString()
+            }
+          })
+
+          // When
+          let middlewarePromise = tripMiddleware.exists(request, response, next)
+
+          // Then
+          middlewarePromise.finally(() => {
+            next.should.not.have.been.called
+            response.statusCode.should.be.equal(404)
+            done()
+          })
+        })
+      })
+
+      describe('when the steps exists', () => {
+        let step_id, trip_id
+        before((done) => {
+          trip_id = mongoose.Types.ObjectId()
+          Step
+            .create({ trip_id, message: Faker.lorem.sentence() })
+            .then((step) => {
+              step_id = step._id
+              done()
+            })
+        })
+
+        it('should call next', (done) => {
+          // Given
+          var next = sinon.spy()
+          let response = httpMocks.createResponse()
+          let request = httpMocks.createRequest({
+            url: '',
+            params: {
+              tripid: trip_id.toString(),
+              stepid: step_id.toString()
+            }
+          })
+
+          // When
+          let middlewarePromise = tripMiddleware.exists(request, response, next)
+
+          // Then
+          middlewarePromise.finally(() => {
+            next.should.have.been.called
+            done()
+          })
+        })
+      })
+    })
+  })
+})
