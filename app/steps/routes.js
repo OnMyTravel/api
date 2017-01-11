31191c4cfa63a55e0a584bcf7bf4d65ac2bb6708
Adrien SAUNIER <asaunier@octo.com>
2017-01-11 09:33:33 +0100

    feat(#10): DELETE /trips/:tripid/steps/:stepid

--- app/steps/routes.js
+++ app/steps/routes.js
@@ -5,10 +5,12 @@ const router = express.Router({mergeParams: true})
 
 const controller = require('./controller')
 const shared = require('../shared')
+const middleware = require('./middleware')
 const tripMiddleware = require('../trips/middleware')
 
 // Routes
 router.get('/', tripMiddleware.exists, controller.get)
 router.post('/', shared.isAuthenticated, tripMiddleware.existsAndIsEditable, controller.create)
+router.delete('/:stepid', shared.isAuthenticated, tripMiddleware.existsAndIsEditable, middleware.exists, controller.deleteOne)
 
 module.exports = router
