52e225fec4624a857db99fe25412b360f1e2b535
Adrien SAUNIER <asaunier@octo.com>
2017-03-07 09:32:04 +0100

    feat(#27): Adding a log where we are unable to delete a file

--- test/steps/controller.unit.spec.js
+++ test/steps/controller.unit.spec.js
@@ -37,9 +37,13 @@ uploadToStorageStub.withArgs(sinon.match.any, 'GOOD_ID')
 uploadToStorageStub.withArgs(sinon.match.any, 'ERRORED_ID')
   .returns(new Promise((resolve, reject) => { reject(new ContainerError('Unable to upload the file')) }))
 
+let findByTripIdAndStepIdStub = sinon.stub()
+let deleteByIdStub = sinon.stub().returns(Promise.resolve())
 let downloadStub = sinon.stub()
+let deleteFileStub = sinon.stub()
 let fsUnlinkSyncStub = sinon.stub()
-let addImageToGalleryStub = sinon.stub().returns(new Promise(function (resolve, reject) { resolve(STEP_SAVED_IN_DB) }))
+let addImageToGalleryStub = sinon.stub().returns(Promise.resolve(STEP_SAVED_IN_DB))
+let winstonErrorStub = sinon.stub()
 let mocks = {
   '../shared': {
     images: {
@@ -47,14 +51,20 @@ let mocks = {
     },
     containers: {
       uploadToStorage: uploadToStorageStub,
-      download: downloadStub
+      download: downloadStub,
+      deleteFile: deleteFileStub
     }
   },
   fs: {
     unlinkSync: fsUnlinkSyncStub
   },
   './repository': {
-    addImageToGallery: addImageToGalleryStub
+    addImageToGallery: addImageToGalleryStub,
+    deleteById: deleteByIdStub,
+    findByTripIdAndStepId: findByTripIdAndStepIdStub
+  },
+  'winston': {
+    error: winstonErrorStub
   }
 }
 const StepController = proxyquire(config.get('app-folder') + '/steps/controller', mocks)
@@ -228,6 +238,125 @@ describe('Steps', () => {
 
         // Then
         downloadStub.should.have.been.calledWith('GOOD_ID', 'GOOD_IMAGE_ID', response)
+      })
+    })
+
+    describe(':deleteOne', () => {
+      let response, request
+      beforeEach(() => {
+        response = httpMocks.createResponse()
+        request = httpMocks.createRequest({
+          method: 'DELETE',
+          url: '',
+          params: {
+            tripid: 'GOOD_ID',
+            stepid: 'GOOD_STEP_ID'
+          }
+        })
+
+        findByTripIdAndStepIdStub.returns(Promise.resolve({
+          gallery: [{
+            source: 'filenumberone',
+            caption: 'My super photo',
+            size: 34567
+          }, {
+            source: 'filenumbertwo',
+            caption: 'My super photo',
+            size: 34567
+          }]
+        }))
+
+        deleteByIdStub.reset()
+        deleteFileStub.reset()
+      })
+
+      it('checks sanity', () => {
+        StepController.deleteOne.should.be.defined
+      })
+
+      it('should remove step from database', () => {
+        // When
+        let promise = StepController.deleteOne(request, response)
+
+        // Then
+        return promise.then(() => {
+          deleteByIdStub.should.have.been.calledWith('GOOD_STEP_ID')
+        })
+      })
+
+      it('should remove file from container', () => {
+        // When
+        let promise = StepController.deleteOne(request, response)
+
+        // Then
+        return promise.then(() => {
+          findByTripIdAndStepIdStub.should.have.been.calledWith('GOOD_ID', 'GOOD_STEP_ID')
+          deleteFileStub.should.have.been.calledWith('GOOD_ID', 'filenumberone')
+        })
+      })
+
+      it('should remove every file from container', () => {
+        // When
+        let promise = StepController.deleteOne(request, response)
+
+        // Then
+        return promise.then(() => {
+          findByTripIdAndStepIdStub.should.have.been.calledWith('GOOD_ID', 'GOOD_STEP_ID')
+          deleteFileStub.should.have.been.calledWith('GOOD_ID', 'filenumberone')
+          deleteFileStub.should.have.been.calledWith('GOOD_ID', 'filenumbertwo')
+        })
+      })
+
+      it('should send JSON result', () => {
+        // When
+        let promise = StepController.deleteOne(request, response)
+
+        // Then
+        return promise.then(() => {
+          response.statusCode.should.equal(200)
+          response.getHeader('Content-Type').should.equal('application/json')
+        })
+      })
+
+      it('should not delete anything if there is no step found', () => {
+        // Given
+        findByTripIdAndStepIdStub.returns(Promise.resolve())
+
+        // When
+        let promise = StepController.deleteOne(request, response)
+
+        // Then
+        return promise.then(() => {
+          deleteByIdStub.should.not.have.been.called
+        })
+      })
+
+      it('should return NOT_FOUND when not step found', () => {
+        // Given
+        findByTripIdAndStepIdStub.returns(Promise.resolve())
+
+        // When
+        let promise = StepController.deleteOne(request, response)
+
+        // Then
+        return promise.then(() => {
+          response.statusCode.should.equal(404)
+          response.getHeader('Content-Type').should.equal('application/json')
+        })
+      })
+
+      it('should log the deletion fail when it occurs', () => {
+        // Given
+        deleteFileStub.onCall(0).returns(Promise.reject(new Error('BUI')))
+        deleteFileStub.onCall(1).returns(Promise.reject(new Error('FGD')))
+
+        // When
+        let promise = StepController.deleteOne(request, response)
+
+        // Then
+        return promise.then(() => {
+          winstonErrorStub.should.have.been.called
+        })
       })
     })
   })
