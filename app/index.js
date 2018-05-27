e064dbb05a737e9dc98693992b10c46c854fdc21
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-05-27 12:48:51 +0200

    Fixing errors on CORS problems

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const config = require('config');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 
 if (config.get('app.logs.enabled')) {
