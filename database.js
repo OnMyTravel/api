10b2b3f1a107f2c289e3bf9766f8ecf5696650ad
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-07-24 13:18:29 +0200

    Using the new configuration for the mongo client

--- database.js
+++ database.js
@@ -8,6 +8,7 @@ const options = {
   poolSize: 10, // Maintain up to 10 socket connections
   // If not connected, return errors immediately rather than waiting for reconnect
   bufferMaxEntries: 0,
+  useNewUrlParser: true
 }
 
 module.exports = {
