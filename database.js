0b956620b16f493e718c4b7197d6b8c7a16051d3
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2018-07-24 13:16:09 +0200

    Updating packages that are outdated

--- database.js
+++ database.js
@@ -7,7 +7,7 @@ const options = {
   reconnectInterval: 500, // Reconnect every 500ms
   poolSize: 10, // Maintain up to 10 socket connections
   // If not connected, return errors immediately rather than waiting for reconnect
-  bufferMaxEntries: 0
+  bufferMaxEntries: 0,
 }
 
 module.exports = {
